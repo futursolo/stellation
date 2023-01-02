d53937ef36e45b3581fa47ac79703cbed83a4658
Kaede Hoshikawa <futursolo@icloud.com>
2023-01-02 23:34:06 +0900

    doc: document stackable-core

--- crates/stackable-core/src/dev.rs
+++ crates/stackable-core/src/dev.rs
@@ -1,7 +1,13 @@
+//! Development Server Utilities.
+
 use std::path::PathBuf;
 
 use serde::{Deserialize, Serialize};
 
+/// Development server metadata.
+///
+/// This information is passed from stackctl to the server when it is started as a development
+/// server.
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct StackctlMetadata {
     pub listen_addr: String,
@@ -9,12 +15,15 @@ pub struct StackctlMetadata {
 }
 
 impl StackctlMetadata {
+    /// The environment variable used by metadata.
     pub const ENV_NAME: &str = "STACKCTL_METADATA";
 
+    /// Parses the metadata from a json string.
     pub fn from_json(s: &str) -> serde_json::Result<Self> {
         serde_json::from_str(s)
     }
 
+    /// Serialises the metadata to a json string.
     pub fn to_json(&self) -> serde_json::Result<String> {
         serde_json::to_string(self)
     }
