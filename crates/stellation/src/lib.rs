b01e73191e73854b27357b5f9e5a1916ae316a01
Kaede Hoshikawa <futursolo@users.noreply.github.com>
2023-01-16 01:52:42 +0900

    feat: add docs crate (#48)
    * feat: add docs crate
    
    * docs(templates): add readme
    
    * ci: remove extra waits

---
+++ crates/stellation/src/lib.rs
@@ -0,0 +1,17 @@
+//! Framework experience for Yew.
+//!
+//! Stellation provides a Yew development stack with:
+//!
+//! 1. Tooling around Server-side Rendering Support.
+//! 2. An easy-to-use, SSR-transparent RPC Implementation.
+//! 3. A development server that automatically rebuild upon changes.
+//! 4. A single binary distribution with embedded frontend.
+//!
+//! # Components
+//!
+//! 1. [stellation-frontend](stellation_frontend): The frontend application.
+//! 2. [stellation-backend](stellation_backend): The backend server.
+//! 3. [stellation-bridge](stellation_bridge): The bridge that provides communication between
+//!   frontend and backend.
+//! 4. [stctl]: Command line utility for building and serving development
+//! server.
