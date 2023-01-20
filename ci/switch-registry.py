5d5937472afa3c69edc84f7906a93ab4d57fe871
Kaede Hoshikawa <futursolo@users.noreply.github.com>
2023-01-20 23:09:17 +0900

    ci(publish): use automated script (#63)
    * ci(publish): use automated script
    
    * ci(main): fix syntax error
    
    * feat(template): embed ci used crates as well
    
    * chore(ci): update auto script
    
    * chore(ci): add more logging
    
    * fix(ci): update auto script
    
    * ci: use sccache
    
    * ci: use config without ext
    
    * ci: update sccache to latest

---
+++ ci/switch-registry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import tomlkit
+import glob
+from pathlib import Path
+
+def main() -> None:
+    cwd = Path.cwd()
+    print(f"Running in {cwd}...")
+
+    for cargo_toml_path in cwd.glob("crates/*/Cargo.toml"):
+        cfg = tomlkit.loads(cargo_toml_path.open().read())
+        print(f"Updating {cargo_toml_path}...")
+
+        for (key, value) in cfg["dependencies"].items():
+            if key != "stctl" and not key.startswith("stellation-"):
+                print(f"  Skipping {key}...")
+                continue
+
+            print(f"  Updating {key}...")
+
+            value["registry"] = "dry-run"
+
+        with cargo_toml_path.open("w") as f:
+            f.write(tomlkit.dumps(cfg))
+            f.flush()
+
+if __name__ == '__main__':
+    main()
