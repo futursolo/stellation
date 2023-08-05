7fa2135d466844cfd4afe1a09a726962ab4b309d
Kaede Hoshikawa <futursolo@icloud.com>
2023-08-05 19:26:28 +0900

    feat(bridge): add mutation state

--- crates/stellation-bridge/src/hooks/mod.rs
+++ crates/stellation-bridge/src/hooks/mod.rs
@@ -3,5 +3,7 @@
 mod use_bridged_mutation;
 mod use_bridged_query;
 
-pub use use_bridged_mutation::{use_bridged_mutation, UseBridgedMutationHandle};
+pub use use_bridged_mutation::{
+    use_bridged_mutation, BridgedMutationState, UseBridgedMutationHandle,
+};
 pub use use_bridged_query::{use_bridged_query, UseBridgedQueryHandle};
--- crates/stellation-bridge/src/hooks/use_bridged_mutation.rs
+++ crates/stellation-bridge/src/hooks/use_bridged_mutation.rs
@@ -3,13 +3,71 @@ use std::marker::PhantomData;
 use std::rc::Rc;
 
 use async_trait::async_trait;
-use bounce::query::{use_mutation, UseMutationHandle};
+use bounce::query::{use_mutation, MutationState, UseMutationHandle};
 use bounce::BounceStates;
 use yew::prelude::*;
 
 use crate::links::Link;
 use crate::routines::{BridgedMutation, MutationResult};
 use crate::state::BridgeSelector;
+
+/// Bridged Mutation State
+#[derive(Debug, PartialEq)]
+pub enum BridgedMutationState<T>
+where
+    T: BridgedMutation + 'static,
+{
+    /// The mutation has not started yet.
+    Idle,
+    /// The mutation is loading.
+    Loading,
+    /// The mutation has completed.
+    Completed {
+        /// Result of the completed mutation.
+        result: MutationResult<T>,
+    },
+    /// A previous mutation has completed and a new mutation is currently loading.
+    Refreshing {
+        /// Result of last completed mutation.
+        last_result: MutationResult<T>,
+    },
+}
+
+impl<T> Clone for BridgedMutationState<T>
+where
+    T: BridgedMutation + 'static,
+{
+    fn clone(&self) -> Self {
+        match self {
+            Self::Idle => Self::Idle,
+            Self::Loading => Self::Loading,
+            Self::Completed { result } => Self::Completed {
+                result: result.clone(),
+            },
+            Self::Refreshing { last_result } => Self::Refreshing {
+                last_result: last_result.clone(),
+            },
+        }
+    }
+}
+
+impl<T> PartialEq<&BridgedMutationState<T>> for BridgedMutationState<T>
+where
+    T: BridgedMutation + 'static,
+{
+    fn eq(&self, other: &&BridgedMutationState<T>) -> bool {
+        self == *other
+    }
+}
+
+impl<T> PartialEq<BridgedMutationState<T>> for &'_ BridgedMutationState<T>
+where
+    T: BridgedMutation + 'static,
+{
+    fn eq(&self, other: &BridgedMutationState<T>) -> bool {
+        *self == other
+    }
+}
 
 struct BridgedMutationInner<M, L>
 where
@@ -61,6 +119,7 @@ where
     L: 'static + Link,
 {
     inner: UseMutationHandle<BridgedMutationInner<T, L>>,
+    state: Rc<BridgedMutationState<T>>,
 }
 
 impl<T, L> UseBridgedMutationHandle<T, L>
@@ -71,6 +130,11 @@ where
     /// Runs a mutation with input.
     pub async fn run(&self, input: impl Into<Rc<T::Input>>) -> MutationResult<T> {
         self.inner.run(input).await?.inner.clone()
+    }
+
+    /// Returns the state of current mutation.
+    pub fn state(&self) -> &BridgedMutationState<T> {
+        self.state.as_ref()
     }
 
     /// Returns the result of last finished mutation (if any).
@@ -94,7 +158,7 @@ where
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("UseBridgedMutationHandle")
-            .field("state", &self.result())
+            .field("state", &self.state())
             .finish()
     }
 }
@@ -107,6 +171,7 @@ where
     fn clone(&self) -> Self {
         Self {
             inner: self.inner.clone(),
+            state: self.state.clone(),
         }
     }
 }
@@ -119,6 +184,28 @@ where
     L: 'static + Link,
 {
     let handle = use_mutation::<BridgedMutationInner<T, L>>();
+    let state = use_memo(
+        |state| match state {
+            MutationState::Idle => BridgedMutationState::Idle,
+            MutationState::Loading => BridgedMutationState::Loading,
+            MutationState::Completed { result } => BridgedMutationState::Completed {
+                result: result
+                    .as_ref()
+                    .map_err(|e| e.clone())
+                    .and_then(|m| m.inner.clone()),
+            },
+            MutationState::Refreshing { last_result } => BridgedMutationState::Refreshing {
+                last_result: last_result
+                    .as_ref()
+                    .map_err(|e| e.clone())
+                    .and_then(|m| m.inner.clone()),
+            },
+        },
+        handle.state().clone(),
+    );
 
-    UseBridgedMutationHandle { inner: handle }
+    UseBridgedMutationHandle {
+        inner: handle,
+        state,
+    }
 }
